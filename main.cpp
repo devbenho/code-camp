5d7b9edda846b85a571b962b921faedceb40a3a1
Muhammad Banhawy <devbenho@gmail.com>
2024-12-11 15:57:32 +0200

    hello

--- main.cpp
+++ main.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
-#include "mathlib.h" 
 using namespace std;
 
 int main()
 {
-    // General Formula is FunctionName<DataType>(arg1, arg2)
-    int num1, num2;
-    cout << "Enter 1st Number: ";
-    cin >> num1; 
-    cout << "Enter 2nd Number: ";
-    cin >> num2; 
-    cout << "Sum: " << sum<int>(num1, num2) << endl;
-    cout << "Subtraction: " << sub<int>(num1, num2) << endl;
-    cout << "Multiplication: " << multiply<int>(num1, num2) << endl;
-    cout << "Division: " << div<int>(num1, num2) << endl;
-
+    cout << "Welcome to The Code Camp!" << endl;
     return 0;
 }
